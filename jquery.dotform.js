29a4530e0e6023d404fa41b1368a244bed945247
billbarsch <billbarsch@gmail.com>
2015-02-18 05:12:07 -0300

    Create jquery.dotform.js

---
+++ jquery.dotform.js
@@ -0,0 +1,44 @@
+jQuery.fn.dotform = function(action){
+	function cria_chave(form,caminho,valor){
+		chaves = caminho.split(".");
+		var nivel_acessado = form[form.length-1].children;
+		for (var i = 0; i < chaves.length; i++){
+			//verifica se está na ponta final do caminho
+			if(i==(chaves.length-1)){
+				//verifica se caminho existe/é array
+				if(Array.isArray(nivel_acessado[chaves[i]])){
+					nivel_acessado[chaves[i]].push({value:valor,
+						children:{}
+					});
+				}else{
+					nivel_acessado[chaves[i]] = [{value:valor,
+						children:{}
+					}]
+				}
+			}
+			ultimo = nivel_acessado[chaves[i]].length-1;
+			var estou_em = nivel_acessado[chaves[i]][ultimo].children;
+			nivel_acessado = estou_em;
+		}
+	}
+	function gera_formulario(elemento_form){
+		var form_name = "form";
+		if(typeof $(elemento_form).name !='undefined')
+		form_name = $(elemento_form).name;
+
+		var form = [{value:form_name,
+			children:{}
+		}];
+		//busca campos e sai criando chaves...
+		$(elemento_form).find(".dotfield").each(function(){
+			cria_chave(form,$(this).attr("dotname"),$(this).val());
+		});
+		return form;
+	}
+	
+	var json = null;
+	this.each(function(){
+		json = gera_formulario($(this));
+    });
+	return json;
+};
