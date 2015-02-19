8e6b9669fafede92289736f86f71b2ba2ccf64d0
Bill Barsch <billbarsch@gmail.com>
2015-02-19 01:06:17 -0300

    alteração na estrutura do json resultante
    agora a estrutura resultante não mostra mais chindrens={} (em branco) ou chaves array com apenas um elemento

--- jquery.dotform.js
+++ jquery.dotform.js
@@ -1,22 +1,30 @@
 jQuery.fn.dotform = function(action){
 	function cria_chave(form,caminho,valor){
 		chaves = caminho.split(".");
-		var nivel_acessado = form.children;
+		var nivel_acessado = form;
 		for (var i = 0; i < chaves.length; i++){
 			//verifica se está na ponta final do caminho
 			if(i==(chaves.length-1)){
 				//verifica se caminho existe/é array
-				if(Array.isArray(nivel_acessado[chaves[i]])){
-					nivel_acessado[chaves[i]].push({value:valor
-					});
+				if(typeof nivel_acessado.children=='undefined') nivel_acessado.children = {};
+				if(typeof nivel_acessado.children[chaves[i]] == 'undefined'){
+					nivel_acessado.children[chaves[i]] = {value:valor};
 				}else{
-					nivel_acessado[chaves[i]] = [{value:valor
-					}];
+					if(Array.isArray(nivel_acessado.children[chaves[i]])){
+						nivel_acessado.children[chaves[i]].push({value:valor});
+					}else{
+						//transforma em array
+						var antigo = nivel_acessado.children[chaves[i]];
+						nivel_acessado.children[chaves[i]] = [antigo,{value:valor}]
+					}
 				}
 			}
-			ultimo = nivel_acessado[chaves[i]].length-1;
-			nivel_acessado[chaves[i]][ultimo].children = {};
-			var estou_em = nivel_acessado[chaves[i]][ultimo].children;
+			if(Array.isArray(nivel_acessado.children[chaves[i]])){
+				ultimo = nivel_acessado.children[chaves[i]].length-1;
+				var estou_em = nivel_acessado.children[chaves[i]][ultimo];
+			}else{
+				var estou_em = nivel_acessado.children[chaves[i]];
+			}
 			nivel_acessado = estou_em;
 		}
 	}
